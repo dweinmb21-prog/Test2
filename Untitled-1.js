36767f16490fa5792e09b09d0940ce47460f5e49
dweinmb21-prog <dweinmb21@gmail.com>
2025-10-16 22:31:34 +0200

    Test2

---
+++ Untitled-1.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = 'my-pwa-cache-v3'; // Zwiększona wersja!
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/avatar.png',
+  '/flag-icon.png', // NOWY PLIK
+  // Dodaj inne pliki ikon, jeśli ich używasz:
+  '/icon-confirm.png',
+  '/icon-data.png',
+  '/icon-lock.png',
+  '/icon-more.png',
+  '/card-bg.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName); // Usuń stare cache
+          }
+        })
+      );
+    })
+  );
+});
